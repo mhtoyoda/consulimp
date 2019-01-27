3cb5e6fdaa92fdf1368fac0dbcb5020a649b79bf
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2019-01-27 16:16:58 -0200

    Refactoring telas

--- src/Educacao.js
+++ src/Educacao.js
@@ -2,90 +2,103 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text, TextInput, ScrollView, FlatList } from 'react-native';
 import { AirbnbRating } from 'react-native-ratings';
 
-const educacao = [
-    { "id": 1, "name": "Academia", "nota": 0 },
-    { "id": 2, "name": "Área de Lazer - Pátios", "nota": 0 },
-	{ "id": 3, "name": "Banheiros - Limpeza / abastecimento", "nota": 0 },
-    { "id": 4, "name": "Bebedouros", "nota": 0 },
-    { "id": 5, "name": "Capachos", "nota": 0 },
-    { "id": 6, "name": "Cestos de Lixo - comum / seletivo", "nota": 0 },
-	{ "id": 7, "name": "Cozinha", "nota": 0 },
-    { "id": 8, "name": "D.M.L", "nota": 0 },
-    { "id": 9, "name": "Elevadores", "nota": 0 },
-    { "id": 10, "name": "Equipamentos de Incêndio", "nota": 0 },   
-    { "id": 11, "name": "Escadas", "nota": 0 },
-    { "id": 12, "name": "Guarita", "nota": 0 },
-    { "id": 13, "name": "Hall Social", "nota": 0 },
-    { "id": 14, "name": "Janelas (face interna)", "nota": 0 },
-    { "id": 15, "name": "Móveis", "nota": 0 },
-    { "id": 16, "name": "Paredes", "nota": 0 },
-    { "id": 17, "name": "Piscina", "nota": 0 },
-    { "id": 18, "name": "Pisos", "nota": 0 },
-    { "id": 19, "name": "Playground - Brinquedoteca", "nota": 0 },
-	{ "id": 20, "name": "Refeitório", "nota": 0 },
-    { "id": 21, "name": "Sala de Aula", "nota": 0 },    
-    { "id": 22, "name": "Vestiários", "nota": 0 }
-]
+export default class Educacao extends Component {
 
-const educacao_equipe = [
-    { "id": 1, "name": "Acessórios/ Equipamentos", "nota": 0 },
-    { "id": 2, "name": "Crachá", "nota": 0 },
-	{ "id": 3, "name": "EPI's", "nota": 0 },
-	{ "id": 4, "name": "Produtos - Diluição", "nota": 0 },
-    { "id": 5, "name": "Produtos - Gestão de Estoques", "nota": 0 },
-	{ "id": 6, "name": "Postura Profissional", "nota": 0 },
-    { "id": 7, "name": "Qualidade Operacional", "nota": 0 },
-	{ "id": 8, "name": "Relacionamento / Cliente", "nota": 0 },
-    { "id": 9, "name": "Uniformes", "nota": 0 }
-]
+    constructor() {
+        super();
+        this.state = {
+            cliente: '',
+            posto: '',
+            supervisor: '',
+            data: '',
+            educacao: [
+                { "id": 1, "name": "Academia", "nota": 0 },
+                { "id": 2, "name": "Área de Lazer - Pátios", "nota": 0 },
+                { "id": 3, "name": "Banheiros - Limpeza / abastecimento", "nota": 0 },
+                { "id": 4, "name": "Bebedouros", "nota": 0 },
+                { "id": 5, "name": "Capachos", "nota": 0 },
+                { "id": 6, "name": "Cestos de Lixo - comum / seletivo", "nota": 0 },
+                { "id": 7, "name": "Cozinha", "nota": 0 },
+                { "id": 8, "name": "D.M.L", "nota": 0 },
+                { "id": 9, "name": "Elevadores", "nota": 0 },
+                { "id": 10, "name": "Equipamentos de Incêndio", "nota": 0 },
+                { "id": 11, "name": "Escadas", "nota": 0 },
+                { "id": 12, "name": "Guarita", "nota": 0 },
+                { "id": 13, "name": "Hall Social", "nota": 0 },
+                { "id": 14, "name": "Janelas (face interna)", "nota": 0 },
+                { "id": 15, "name": "Móveis", "nota": 0 },
+                { "id": 16, "name": "Paredes", "nota": 0 },
+                { "id": 17, "name": "Piscina", "nota": 0 },
+                { "id": 18, "name": "Pisos", "nota": 0 },
+                { "id": 19, "name": "Playground - Brinquedoteca", "nota": 0 },
+                { "id": 20, "name": "Refeitório", "nota": 0 },
+                { "id": 21, "name": "Sala de Aula", "nota": 0 },
+                { "id": 22, "name": "Vestiários", "nota": 0 }
+            ],
+            educacao_equipe: [
+                { "id": 1, "name": "Acessórios/ Equipamentos", "nota": 0 },
+                { "id": 2, "name": "Crachá", "nota": 0 },
+                { "id": 3, "name": "EPI's", "nota": 0 },
+                { "id": 4, "name": "Produtos - Diluição", "nota": 0 },
+                { "id": 5, "name": "Produtos - Gestão de Estoques", "nota": 0 },
+                { "id": 6, "name": "Postura Profissional", "nota": 0 },
+                { "id": 7, "name": "Qualidade Operacional", "nota": 0 },
+                { "id": 8, "name": "Relacionamento / Cliente", "nota": 0 },
+                { "id": 9, "name": "Uniformes", "nota": 0 }
+            ]
+        }
+    }
 
-const Educacao = () => (
-    <ScrollView>
-        <View>
-            <Text style={styles.text_field}>Cliente:</Text>
-            <TextInput style={{ height: 40 }}></TextInput>
-        </View>
-        <View>
-            <Text style={styles.text_field}>Posto:</Text>
-            <TextInput style={{ height: 40 }}></TextInput>
-        </View>
-        <View>
-            <Text style={styles.text_field}>Supervisor:</Text>
-            <TextInput style={{ height: 40 }}></TextInput>
-        </View>
-        <View>
-            <Text style={styles.text_field}>Data:</Text>
-            <TextInput style={{ height: 40 }}></TextInput>
-        </View>
-        <View>
-            <FlatList
-                keyExtractor={item => item.id.toString()}
-                data={educacao}
-                renderItem={({ item }) =>
-                    <View style={styles.item}>
-                        <Text style={styles.text}>{item.name}</Text>
-                        <AirbnbRating count={4} defaultRating={0} reviews={['Ruim', 'Regular', 'Bom', 'Ótimo']} size={25}></AirbnbRating>
-                    </View>
-                }
-            />
-        </View>
-        <View>
-            <Text style={styles.text_equipe}>Equipe</Text>
-        </View>
-        <View>
-            <FlatList
-                keyExtractor={item => item.id.toString()}
-                data={educacao_equipe}
-                renderItem={({ item }) =>
-                    <View style={styles.item}>
-                        <Text style={styles.text}>{item.name}</Text>
-                        <AirbnbRating count={4} defaultRating={0} reviews={['Ruim', 'Regular', 'Bom', 'Ótimo']} size={25}></AirbnbRating>
-                    </View>
-                }
-            />
-        </View>
-    </ScrollView>
-);
+    render() {
+        return (
+            <ScrollView>
+                <View>
+                    <Text style={styles.text_field}>Cliente:</Text>
+                    <TextInput style={{ height: 40 }} onChangeText={text => this.setState({ cliente: text })}></TextInput>
+                </View>
+                <View>
+                    <Text style={styles.text_field}>Posto:</Text>
+                    <TextInput style={{ height: 40 }} onChangeText={text => this.setState({ posto: text })}></TextInput>
+                </View>
+                <View>
+                    <Text style={styles.text_field}>Supervisor:</Text>
+                    <TextInput style={{ height: 40 }} onChangeText={text => this.setState({ supervisor: text })}></TextInput>
+                </View>
+                <View>
+                    <Text style={styles.text_field}>Data:</Text>
+                    <TextInput style={{ height: 40 }} onChangeText={text => this.setState({ data: text })}></TextInput>
+                </View>
+                <View>
+                    <FlatList
+                        keyExtractor={item => item.id.toString()}
+                        data={this.state.educacao}
+                        renderItem={({ item }) =>
+                            <View style={styles.item}>
+                                <Text style={styles.text}>{item.name}</Text>
+                                <AirbnbRating count={4} defaultRating={0} reviews={['Ruim', 'Regular', 'Bom', 'Ótimo']} size={25}></AirbnbRating>
+                            </View>
+                        }
+                    />
+                </View>
+                <View>
+                    <Text style={styles.text_equipe}>Equipe</Text>
+                </View>
+                <View>
+                    <FlatList
+                        keyExtractor={item => item.id.toString()}
+                        data={this.state.educacao_equipe}
+                        renderItem={({ item }) =>
+                            <View style={styles.item}>
+                                <Text style={styles.text}>{item.name}</Text>
+                                <AirbnbRating count={4} defaultRating={0} reviews={['Ruim', 'Regular', 'Bom', 'Ótimo']} size={25}></AirbnbRating>
+                            </View>
+                        }
+                    />
+                </View>
+            </ScrollView>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     item: {
@@ -117,4 +130,3 @@ const styles = StyleSheet.create({
     }
 });
 
-export default Educacao;
--- src/Saude.js
+++ src/Saude.js
@@ -2,94 +2,107 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text, TextInput, ScrollView, FlatList } from 'react-native';
 import { AirbnbRating } from 'react-native-ratings';
 
-const saude = [
-    { "id": 1, "name": "Administração", "nota": 0 },
-    { "id": 2, "name": "Área Interna", "nota": 0 },
-	{ "id": 3, "name": "Área Externa", "nota": 0 },
-    { "id": 4, "name": "Banheiros - limpeza / abastecimento", "nota": 0 },
-    { "id": 5, "name": "Berçário", "nota": 0 },
-    { "id": 6, "name": "C.M.E", "nota": 0 },
-    { "id": 7, "name": "Centro Cirúrgico", "nota": 0 },
-    { "id": 8, "name": "Centro Obstétrico", "nota": 0 },
-    { "id": 9, "name": "Cestos de Lixo", "nota": 0 },   
-    { "id": 10, "name": "Consultórios", "nota": 0 },
-    { "id": 11, "name": "D.M.L", "nota": 0 },
-    { "id": 12, "name": "Enfermarias", "nota": 0 },
-    { "id": 13, "name": "Equipamentos de Incêndio", "nota": 0 },
-    { "id": 14, "name": "Expurgo", "nota": 0 },
-    { "id": 15, "name": "Farmácia", "nota": 0 },
-    { "id": 16, "name": "Janelas", "nota": 0 },
-    { "id": 17, "name": "Limpeza Concorrente", "nota": 0 },
-    { "id": 18, "name": "Limpeza Terminal", "nota": 0 },
-    { "id": 19, "name": "Necrotério", "nota": 0 },
-    { "id": 20, "name": "Paredes", "nota": 0 },
-    { "id": 21, "name": "Pisos", "nota": 0 },    
-    { "id": 22, "name": "Postos Enfermagem", "nota": 0 },
-	{ "id": 23, "name": "Pronto Atendimento", "nota": 0 },
-	{ "id": 24, "name": "Quartos", "nota": 0 },
-	{ "id": 25, "name": "UTI - Adulto", "nota": 0 },
-	{ "id": 26, "name": "UTI - NEO", "nota": 0 }
-]
+export default class Saude extends Component {
 
-const saude_equipe = [
-    { "id": 1, "name": "Acessórios/ Equipamentos", "nota": 0 },
-    { "id": 2, "name": "Crachá", "nota": 0 },
-	{ "id": 3, "name": "EPI's", "nota": 0 },
-    { "id": 4, "name": "Postura Profissional", "nota": 0 },
-	{ "id": 5, "name": "Produtos - Diluição", "nota": 0 },	
-    { "id": 6, "name": "Produtos - Gestão de Estoques", "nota": 0 },
-    { "id": 7, "name": "Qualidade Operacional", "nota": 0 },
-	{ "id": 8, "name": "Relacionamento / Cliente", "nota": 0 },
-    { "id": 9, "name": "Uniformes", "nota": 0 }
-]
+    constructor() {
+        super();
+        this.state = {
+            cliente: '',
+            posto: '',
+            supervisor: '',
+            data: '',
+            saude: [
+                { "id": 1, "name": "Administração", "nota": 0 },
+                { "id": 2, "name": "Área Interna", "nota": 0 },
+                { "id": 3, "name": "Área Externa", "nota": 0 },
+                { "id": 4, "name": "Banheiros - limpeza / abastecimento", "nota": 0 },
+                { "id": 5, "name": "Berçário", "nota": 0 },
+                { "id": 6, "name": "C.M.E", "nota": 0 },
+                { "id": 7, "name": "Centro Cirúrgico", "nota": 0 },
+                { "id": 8, "name": "Centro Obstétrico", "nota": 0 },
+                { "id": 9, "name": "Cestos de Lixo", "nota": 0 },
+                { "id": 10, "name": "Consultórios", "nota": 0 },
+                { "id": 11, "name": "D.M.L", "nota": 0 },
+                { "id": 12, "name": "Enfermarias", "nota": 0 },
+                { "id": 13, "name": "Equipamentos de Incêndio", "nota": 0 },
+                { "id": 14, "name": "Expurgo", "nota": 0 },
+                { "id": 15, "name": "Farmácia", "nota": 0 },
+                { "id": 16, "name": "Janelas", "nota": 0 },
+                { "id": 17, "name": "Limpeza Concorrente", "nota": 0 },
+                { "id": 18, "name": "Limpeza Terminal", "nota": 0 },
+                { "id": 19, "name": "Necrotério", "nota": 0 },
+                { "id": 20, "name": "Paredes", "nota": 0 },
+                { "id": 21, "name": "Pisos", "nota": 0 },
+                { "id": 22, "name": "Postos Enfermagem", "nota": 0 },
+                { "id": 23, "name": "Pronto Atendimento", "nota": 0 },
+                { "id": 24, "name": "Quartos", "nota": 0 },
+                { "id": 25, "name": "UTI - Adulto", "nota": 0 },
+                { "id": 26, "name": "UTI - NEO", "nota": 0 }
+            ],
+            saude_equipe: [
+                { "id": 1, "name": "Acessórios/ Equipamentos", "nota": 0 },
+                { "id": 2, "name": "Crachá", "nota": 0 },
+                { "id": 3, "name": "EPI's", "nota": 0 },
+                { "id": 4, "name": "Postura Profissional", "nota": 0 },
+                { "id": 5, "name": "Produtos - Diluição", "nota": 0 },
+                { "id": 6, "name": "Produtos - Gestão de Estoques", "nota": 0 },
+                { "id": 7, "name": "Qualidade Operacional", "nota": 0 },
+                { "id": 8, "name": "Relacionamento / Cliente", "nota": 0 },
+                { "id": 9, "name": "Uniformes", "nota": 0 }
+            ]
+        };
+    }
 
-const Saude = () => (
-    <ScrollView>
-        <View>
-            <Text style={styles.text_field}>Cliente:</Text>
-            <TextInput style={{ height: 40 }}></TextInput>
-        </View>
-        <View>
-            <Text style={styles.text_field}>Posto:</Text>
-            <TextInput style={{ height: 40 }}></TextInput>
-        </View>
-        <View>
-            <Text style={styles.text_field}>Supervisor:</Text>
-            <TextInput style={{ height: 40 }}></TextInput>
-        </View>
-        <View>
-            <Text style={styles.text_field}>Data:</Text>
-            <TextInput style={{ height: 40 }}></TextInput>
-        </View>
-        <View>
-            <FlatList
-                keyExtractor={item => item.id.toString()}
-                data={saude}
-                renderItem={({ item }) =>
-                    <View style={styles.item}>
-                        <Text style={styles.text}>{item.name}</Text>
-                        <AirbnbRating count={4} defaultRating={0} reviews={['Ruim', 'Regular', 'Bom', 'Ótimo']} size={25}></AirbnbRating>
-                    </View>
-                }
-            />
-        </View>
-        <View>
-            <Text style={styles.text_equipe}>Equipe</Text>
-        </View>
-        <View>
-            <FlatList
-                keyExtractor={item => item.id.toString()}
-                data={saude_equipe}
-                renderItem={({ item }) =>
-                    <View style={styles.item}>
-                        <Text style={styles.text}>{item.name}</Text>
-                        <AirbnbRating count={4} defaultRating={0} reviews={['Ruim', 'Regular', 'Bom', 'Ótimo']} size={25}></AirbnbRating>
-                    </View>
-                }
-            />
-        </View>
-    </ScrollView>
-);
+    render() {
+        return (
+            <ScrollView>
+                <View>
+                    <Text style={styles.text_field}>Cliente:</Text>
+                    <TextInput style={{ height: 40 }} onChangeText={text => this.setState({ cliente: text })}></TextInput>
+                </View>
+                <View>
+                    <Text style={styles.text_field}>Posto:</Text>
+                    <TextInput style={{ height: 40 }} onChangeText={text => this.setState({ posto: text })}></TextInput>
+                </View>
+                <View>
+                    <Text style={styles.text_field}>Supervisor:</Text>
+                    <TextInput style={{ height: 40 }} onChangeText={text => this.setState({ supervisor: text })}></TextInput>
+                </View>
+                <View>
+                    <Text style={styles.text_field}>Data:</Text>
+                    <TextInput style={{ height: 40 }} onChangeText={text => this.setState({ data: text })}></TextInput>
+                </View>
+                <View>
+                    <FlatList
+                        keyExtractor={item => item.id.toString()}
+                        data={this.state.saude}
+                        renderItem={({ item }) =>
+                            <View style={styles.item}>
+                                <Text style={styles.text}>{item.name}</Text>
+                                <AirbnbRating count={4} defaultRating={0} reviews={['Ruim', 'Regular', 'Bom', 'Ótimo']} size={25}></AirbnbRating>
+                            </View>
+                        }
+                    />
+                </View>
+                <View>
+                    <Text style={styles.text_equipe}>Equipe</Text>
+                </View>
+                <View>
+                    <FlatList
+                        keyExtractor={item => item.id.toString()}
+                        data={this.state.saude_equipe}
+                        renderItem={({ item }) =>
+                            <View style={styles.item}>
+                                <Text style={styles.text}>{item.name}</Text>
+                                <AirbnbRating count={4} defaultRating={0} reviews={['Ruim', 'Regular', 'Bom', 'Ótimo']} size={25}></AirbnbRating>
+                            </View>
+                        }
+                    />
+                </View>
+            </ScrollView>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     item: {
@@ -120,4 +133,3 @@ const styles = StyleSheet.create({
         marginTop: 5,
     }
 });
-export default Saude;
