81d6b2d8b54a1e348dedbecdd8c85fffcb0ce5a3
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2019-02-22 15:42:42 -0300

    Refactoring de componente css

--- src/screen/Geral.js
+++ src/screen/Geral.js
@@ -6,6 +6,7 @@ import 'moment/locale/pt-br';
 import XLSX from 'xlsx';
 import File from '../function/File';
 import Calculo from '../function/Calculo';
+import styles from '../Style';
 
 export default class Geral extends Component {
 
@@ -168,47 +169,3 @@ export default class Geral extends Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    item: {
-        height: 70,
-        paddingHorizontal: 15,
-        borderWidth: 0.5,
-        borderColor: '#222',
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    text: {
-        fontSize: 12,
-        fontWeight: 'bold',
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 20,
-        textAlign: 'center',
-        padding: 15,
-    },
-    text_equipe: {
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    text_field: {
-        fontWeight: 'bold',
-        marginTop: 5,
-    },
-    button: {
-        alignItems: 'center',
-        backgroundColor: '#00009C',
-        padding: 10,
-        borderRadius: 30,
-    },
-    textButton: {
-        fontSize: 16,
-        color: '#FFF'
-    },
-    containerButton: {
-        paddingTop: 20,
-        paddingBottom: 20,
-    }
-});
